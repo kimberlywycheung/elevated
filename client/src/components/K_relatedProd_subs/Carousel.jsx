ac5b2fe2111376c596b43908d08c9fddbe3d0a17
Kim <kim@BK-MBP16.local>
2022-12-02 15:04:35 -0800

    adding new carousel edits to: - hold states related to outfits and related product ids - added logic to prevent mapping through empty array (which throws an error) - added temp card for 'add to outfit'

--- client/src/components/K_relatedProd_subs/Carousel.jsx
+++ client/src/components/K_relatedProd_subs/Carousel.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import Comparison from './Comparison.jsx';
 import Card from './Card.jsx';
 
-const Carousel = ({ type, currentState }) => {
-  // types: 'related', 'outfit'
-  // for outfits, save to local storage
+const Carousel = ({ type, currentState, currentProd }) => {
 
-  const title = type === 'related' ? 'Related Products' : 'Your Outfit'
+  // console.log('type: ', type);
+  // console.log('currentState: ', currentState);
+
+  const emptyOutfit = currentState.length === 0 ? true : false;
+  const showAdd = type === 'outfits'? true : false;
+  const title = type === 'outfits' ? 'Your Outfit' : 'Related Products';
+
+  const cards = () => {
+    if (currentState.length > 0) {
+      currentState.map((item) => {
+        return <Card key={item} item={item}/>
+      })
+    }
+  }
 
   return (
     <div>
       <p id={type}>{title}</p>
-      {/* {
+      {showAdd &&
+        <div> <p>Add to Outfit</p> </div> // add className = card?
+      }
+      {currentState.length > 0 &&
         currentState.map((item) => {
           return <Card key={item} item={item}/>
         })
-      } */}
+      }
     </div>
   )
 }
