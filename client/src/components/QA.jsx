7fd7085bfb5a37a24a9f10e1bd3c0acd225abcff
Evan OShea <evanoshea21@gmail.com>
2022-12-02 14:57:52 -0800

    misc

--- client/src/components/QA.jsx
+++ client/src/components/QA.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import axios from 'axios';
+import Search from './E_qa_subs/Search.jsx';
+import QaList from './E_qa_subs/QaList.jsx';
+import QaModal from './E_qa_subs/QaModal.jsx';
 
 
 const QuestionsAnswers = ({productID}) => {
+  const [formStyle, setFormStyle] = React.useState({display:"none"});
+  const [Qlist, setQlist] = React.useState([]);
+
+  React.useEffect(() => {
+    if(productID) {
+      getQlist();
+    }
+  },[productID]);
+
+  const getQlist = () => {
+    var url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions?product_id=${productID}`;
+    axios.get(url, {
+      headers: { 'Authorization': process.env.GITHUB_TOKEN }
+    })
+    .then((response) => {
+      console.log('GetQlist Response->\n', response.data.results);
+      setQlist(response.data.results);
+    })
+  };
+
+  if(!productID) {
+    return (
+      <div>loading...</div>
+    )
+  }
 
   return (
     <div className='questions-answers'>
-      QuestionsAnswers Div
-      {/* add your sub-widgets here */}
-      some edits here
-      SECOND edit here
+      <Search />
+      <QaList list={Qlist}/>
+      <div className='qa-btns'>
+        <button>more answered questions</button>
+        <button onClick={e => {e.preventDefault(); setFormStyle({display: "block"})}}>add question</button>
+      </div>
+      <div style={formStyle}>
+        <QaModal />
+      </div>
     </div>
   )
 }
