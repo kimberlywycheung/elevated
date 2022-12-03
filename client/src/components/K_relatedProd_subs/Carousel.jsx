01ff7f4b7e5442274c96021e70e2105c93f97cd5
Kim <kim@BK-MBP16.local>
2022-12-02 17:11:56 -0800

    updated initial state for favorites in app to be empty string instead of an array used useeffect in related products to set states for outfits and related ids added logic for both outfit and related types of carousels

--- client/src/components/K_relatedProd_subs/Carousel.jsx
+++ client/src/components/K_relatedProd_subs/Carousel.jsx
@@ -3,10 +3,14 @@ import Comparison from './Comparison.jsx';
 import Card from './Card.jsx';
 
 const Carousel = ({ type, currentState, currentProd }) => {
-
+  // convert localstorage string to array
   if (typeof(currentState) === 'string') {
     currentState = currentState.replace(/\r?\n|\r/g, '').split(',');
   }
+
+  // remove any duplicates from currentState
+  let stateSet = [...new Set(currentState)];
+  currentState = Array(stateSet);
 
   const showAdd = type === 'outfits'? true : false;
   const title = type === 'outfits' ? 'Your Outfit' : 'Related Products';
--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,27 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import Carousel from './K_relatedProd_subs/Carousel.jsx';
 
 const RelatedProducts = ({ product }) => {
   const [outfits, setOutfits] = useState([]);
   const [relatedIds, setRelatedIds] = useState([]);
 
-  // console.log('product', product);
-  // console.log('outfits: ', outfits);
-  // console.log('related ids', relatedIds);
-
   useEffect(() => {
     setOutfits(window.localStorage.getItem('favorites'));
   }, [outfits])
 
-  if (product.id) {
-    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${product.id}/related`, {
-      headers: { 'Authorization': process.env.GITHUB_TOKEN }
-    })
-    .then(({ data }) => setRelatedIds(data))
-    .catch((err) => console.log(err));
-  }
+  useEffect(() => {
+    if (product.id) {
+      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${product.id}/related`, {
+        headers: { 'Authorization': process.env.GITHUB_TOKEN }
+      })
+      .then(({ data }) => setRelatedIds(data))
+      .catch((err) => console.log(err));
+    }
+  }, [product])
 
   return (
     <div className='related-products'>
