a97d0ebf9cf5a6dd1b50ca8b21cde59d140ce535
Ryan Gehris <rogehris@gmail.com>
2022-12-02 17:32:50 -0800

    updated reviews.jsx to comply with linter

--- client/src/components/R_ratingsReviews_subs/Reviews.jsx
+++ client/src/components/R_ratingsReviews_subs/Reviews.jsx
@@ -1,30 +1,39 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Reviews = ({productID}) => {
-  const [reviews, setReviews] = useState({})
+const Reviews = function Reviews({ productID }) {
+  const [reviews, setReviews] = useState({});
 
   useEffect(() => {
     if (productID !== undefined) {
       axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=${productID}`, {
-        headers: { 'Authorization': process.env.GITHUB_TOKEN }
+        headers: { Authorization: process.env.GITHUB_TOKEN },
       })
         .then((result) => {
           // console.log('REVIEW State', result.data)
-          setReviews(result.data)
+          setReviews(result.data);
         })
         .catch((err) => {
-          // console.log("Error fetching data in 'components/R_ratingsReviews_subs/Breakdown.jsx'", err)
-          alert('Error in Breakdown.jsx')
-        })
+          // console.log('Error fetching data in "Breakdown.jsx"', err);
+          alert('Error in Breakdown.jsx', err);
+        });
     }
   }, [productID]);
 
   return (
     <div>
       Reviews list, sort, and add review button
+      <span>
+        There are
+        {reviews.count}
+        reviews
+      </span>
     </div>
-  )
-}
+  );
+};
+
+Reviews.propTypes = {
+  productID: PropTypes.number.isRequired,
+};
 
 export default Reviews;
