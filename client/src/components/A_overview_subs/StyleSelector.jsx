6c41dfde4d0269055383cd9a357d577f2cfa8df6
Aristotle Jalalianfard <aristotle.jalalianfard@gmail.com>
2022-12-06 09:54:57 -0800

    test

--- client/src/components/A_overview_subs/StyleSelector.jsx
+++ client/src/components/A_overview_subs/StyleSelector.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Swatch from "./Swatch.jsx";
 
 const StyleSelector = ({ styles, style, setStyle }) => {
-
+//commented out for visibility
   if (style) {
     return (
       <div className="current-style">
         Style {'> ' + style.name}
 
         <div className="style-selector">
-          {styles.map((oneStyle) => { return <Swatch oneStyle={oneStyle} /> })}
+          {/* {styles.map((oneStyle) => { return <Swatch oneStyle={oneStyle} className="swatch" /> })} */}
         </div>
       </div>)
   }
