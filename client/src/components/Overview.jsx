70f64626e9873849847cf4a11608901693bda88f
Aristotle Jalalianfard <aristotle.jalalianfard@gmail.com>
2022-12-06 12:13:07 -0800

    overview fix

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import ImageGallery from './A_overview_subs/ImageGallery.jsx';
+import ProductInfo from './A_overview_subs/productInfo.jsx';
+import StyleSelector from './A_overview_subs/StyleSelector.jsx';
 //testing
 
-const Overview = ({productID}) => {
+const Overview = ({ product }) => {
 
-  return (
-    <div className='overview'>
-      Overview Div
-      {/* add your sub-widgets here */}
-    </div>
-  )
-}
+  const [style, setStyle] = useState(null);
+  const [styles, setStyles] = useState(null);
 
-export default Overview;
+  
+
+
+    const getStyles = async () => {
+
+      if (product) {
+        const { data: { results } } = await axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344/styles`, {
+          headers: { Authorization: process.env.GITHUB_TOKEN }
+        })
+        setStyles(results);
+        setStyle(results[0]);
+
+      }
+      React.useEffect(() => {
+        getStyles();
+      }, []);
+    }
+
+    return (
+      <div className='overview'>
+        Overview Div
+        <ImageGallery />
+        <ProductInfo product={product} style={style} />
+        <StyleSelector styles={styles} />
+      </div>
+    )
+  }
+
+  export default Overview;
