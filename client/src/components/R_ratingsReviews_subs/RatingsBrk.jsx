9bfdb708de2232ba2b306f7e7a18d22bcdbb6631
Ryan Gehris <rogehris@gmail.com>
2022-12-06 14:12:50 -0800

    created new file, helper functions.

--- client/src/components/R_ratingsReviews_subs/RatingsBrk.jsx
+++ client/src/components/R_ratingsReviews_subs/RatingsBrk.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import starArray from '../../helperFunctions/starArray.js'
 
 const RatingsBrk = function RatingsBrk({ breakdown }) {
   const [stats, setStats] = useState({});
@@ -43,53 +44,12 @@ const RatingsBrk = function RatingsBrk({ breakdown }) {
     )
   }
 
-  let rating = stats.ratings.avg || 0;
-  let stars = [];
-  while (stars.length < 5) {
-    if (rating > 1) {
-      stars.push(1);
-    } else if (rating > 0) {
-      let empty = Math.abs(0 - rating);
-      let quart = Math.abs(0.25 - rating);
-      let half = Math.abs(0.5 - rating);
-      let three = Math.abs(0.75 - rating);
-      let full = Math.abs(1 - rating);
-      let closest = Math.min(empty, quart, half, three, full);
-      switch (closest) {
-        case (empty):
-          stars.push(0);
-          break;
-        case quart:
-          stars.push(0.28);
-          break;
-        case half:
-          stars.push(0.5);
-          break;
-        case three:
-          stars.push(0.72);
-          break;
-        case full:
-          stars.push(1.0);
-          break;
-        default:
-          console.log("OOPS");
-          stars.push(0);
-          break;
-      }
-    } else {
-      stars.push(0);
-    }
-    rating = rating - 1;
-  }
-
-  // client/dist/images/star.png
-
   return (
     <div>
       <div>
         <span>{stats.ratings.avg}</span>
         <span>
-          {stars.map((item, i) => {
+          {starArray(stats.ratings.avg).map((item, i) => {
               return (
                   <div className="single-star-container" key={i}>
                       <div className="single-star-fill" style={{"width" : `${parseInt(item*20.3)}px`}}>
--- client/src/components/R_ratingsReviews_subs/ReviewTile.jsx
+++ client/src/components/R_ratingsReviews_subs/ReviewTile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import starArray from '../../helperFunctions/starArray.js'
 
 const ReviewTile = function ReviewTile({ review, setRList }) {
   let date = new Date(review.date)
@@ -19,13 +20,45 @@ const ReviewTile = function ReviewTile({ review, setRList }) {
           console.log('error', err);
         })
     }
-
   };
+
+  const handleReport = (e) => {
+    e.preventDefault()
+    if(window.localStorage.getItem(`reported${review.review_id}`) === null) {
+      window.localStorage.setItem(`reported${review.review_id}`, true);
+      const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/${review.review_id}/report`;
+      const auth = {'Authorization': process.env.GITHUB_TOKEN}
+      axios({method: 'put', url, headers: auth})
+        .then(res => {
+          console.log('reported');
+          console.log('response', res);
+          setRList();
+        })
+        .catch(err => {
+          console.log('error', err);
+        })
+    }
+  }
 
   return (
     <div style={{border: "1px solid red"}}>
-      <span>{review.rating} ★★★★★</span>
-      <span>{date.toLocaleDateString()}</span>
+      <div>
+        <span>
+          <span>{review.rating}</span>
+          <span>
+              {starArray(review.rating).map((item, i) => {
+                  return (
+                      <div className="single-star-container" key={i}>
+                          <div className="single-star-fill" style={{"width" : `${parseInt(item*20.3)}px`}}>
+                              <img className="single-star-outline" src="../../client/dist/images/star2.png" alt="stars alt"></img>
+                          </div>
+                      </div>
+                  );
+              })}
+          </span>
+        </span>
+        <span>{date.toLocaleDateString()}</span>
+      </div>
       <h3>{review.reviewer_name}</h3>
       {review.recommend === true &&
         <div>
@@ -43,7 +76,7 @@ const ReviewTile = function ReviewTile({ review, setRList }) {
           {review.response}
         </div>
       }
-      <span>Helpful? <a onClick={handleVote}>Yes</a> ({review.helpfulness})</span>
+      <span>Helpful? <a onClick={handleVote}>Yes</a> ({review.helpfulness}) | <a onClick={handleReport}>Report</a></span>
     </div>
   )
 }
