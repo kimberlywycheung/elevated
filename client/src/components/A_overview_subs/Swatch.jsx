bc808dafcfd6c1bdaa7c50d34437f5e88c1b4788
Evan OShea <evanoshea21@gmail.com>
2022-12-07 12:02:17 -0800

    styles working

--- client/src/components/A_overview_subs/Swatch.jsx
+++ client/src/components/A_overview_subs/Swatch.jsx
@@ -4,8 +4,9 @@ const Swatch = ({ oneStyle }) => {
 
   let url = oneStyle.photos[0].thumbnail_url;
   return (
-
+    <div className='ov-style-container'>
       <img className="ov-style-select swatch" src={url} />
+    </div>
 
   )
 }
