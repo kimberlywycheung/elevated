6d37ae565b12775254cb83ab670bae90f84b02d7
Ryan Gehris <rogehris@gmail.com>
2022-12-08 17:20:56 -0800

    added correct labels below characteristics bar. Successfully implemented images for form data. Axios request is working

--- client/src/components/R_ratingsReviews_subs/ReviewModal.jsx
+++ client/src/components/R_ratingsReviews_subs/ReviewModal.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import $ from 'jquery';
 
-const ReviewModal = function ReviewModal({ isOpen, name, id, setIsOpen, charBreak }) {
+const ReviewModal = function ReviewModal({ isOpen, name, id, setIsOpen, charBreak, setRList }) {
+  const [image, setImage] = useState('')
+  const [images, setImages] = useState([])
+
+  if (!charBreak) {
+    return null;
+  }
 
 
-  console.log('CharBreak ', charBreak);
   const chars = Object.keys(charBreak.characteristics);
   const charObj = charBreak.characteristics;
+  const charChart = {
+    Size: ['A size too small', '½ a size too small', 'Perfect', '½ a size too big', 'A size too wide'],
+    Width: ['Too narrow', 'Slightly Narrow', 'Perfect', 'Slightly wide', 'Too wide'],
+    Comfort: ['Uncomfortable', 'Slightyly uncomfortable', 'Ok', 'Comfortable', 'Perfect'],
+    Quality: ['Poor', 'Below average', 'What I expected', 'Pretty Great', 'Perfect'],
+    Length: ['Runs Short', 'Runs slightly short', 'Perfect', 'Runs slightly long', 'Runs long'],
+    Fit: ['Runs tight', 'Runs slightly tight', 'Perfect', 'Runs slightly loose', 'Runs loose']
+  }
 
   const formSubmit = function(e) {
     e.preventDefault()
     let newReview = new FormData(e.target);
     let newObj = createParameters(newReview)
-    console.log("New Review", newObj)
+    if (newObj) {
+      const auth = {'Authorization': process.env.GITHUB_TOKEN}
+      const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews`;
+      axios({method: 'post', url: url, headers: auth, data: newObj})
+        .then((res) => {
+          console.log('review POST response', res);
+          setRList();
+        })
+        .catch(err => {
+          console.error('ERROR', err);
+          alert('Didn\'t Post Review' + err);
+        })
+    }
     setIsOpen(false);
+    setImages([]);
   }
 
   const createParameters = function(formData) {
@@ -30,12 +58,25 @@ const ReviewModal = function ReviewModal({ isOpen, name, id, setIsOpen, charBrea
       } else if (key === 'recommend') {
         parameters[key] = (value === 'true')
       } else if (key === 'photos') {
-        console.log('photos')
+        parameters[key] = images;
       } else {
         parameters[key] = value
       }
     }
     return parameters;
+  }
+
+  const imagehandler = function(e, action) {
+    e.preventDefault();
+    let imgUrl = $('#photos').val();
+    const updatedImages = [...images]
+    if (action === 'add') {
+      updatedImages.push(imgUrl)
+    } else if (action === 'del') {
+      updatedImages.pop()
+    }
+    setImage('');
+    setImages(updatedImages);
   }
 
   if (!isOpen) return null
@@ -87,7 +128,7 @@ const ReviewModal = function ReviewModal({ isOpen, name, id, setIsOpen, charBrea
               chars.map((char, index) => {
                 return (
                   <div key={index}>
-                    <label>{char}</label>
+                    <label>{char}: </label>
                     <div>
                       <input type="radio" value="1" name={charObj[char].id} required></input>
                       <label>1</label>
@@ -99,6 +140,10 @@ const ReviewModal = function ReviewModal({ isOpen, name, id, setIsOpen, charBrea
                       <label>4</label>
                       <input type="radio" value="5" name={charObj[char].id}></input>
                       <label>5</label>
+                    </div>
+                    <div>
+                      <span>{charChart[char][0]}</span>
+                      <span>{charChart[char][4]}</span>
                     </div>
                   </div>
                 )
@@ -121,7 +166,24 @@ const ReviewModal = function ReviewModal({ isOpen, name, id, setIsOpen, charBrea
           </div>
           <div>
             <label>Upload Photos</label>
-            <input type="image"></input>
+            <div>
+              <input
+                name="photos"
+                id="photos"
+                type="input"
+                value={image}
+                onChange={(e) => {setImage(e.target.value)}}
+                placeholder={`You can add ${5 - images.length} more images!`}>
+              </input>
+            </div>
+            <div>
+              {images.length < 5 &&
+                <span onClick={(e) => imagehandler(e, 'add')}>Add Image</span>
+              }
+              {images.length > 0 &&
+                <span onClick={(e) => imagehandler(e, 'del')}>Remove Image {images.length}</span>
+              }
+            </div>
           </div>
           <div>
             <label>* Nickname:</label>
